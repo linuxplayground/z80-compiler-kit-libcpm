a92270704a17a15b8dbf44af37277aba32558711
productiondave <kiwilathams@gmail.com>
2025-10-10 13:09:48 +1300

    remove _ from IO port defines

--- include/arch/RETRO/io.h
+++ include/arch/RETRO/io.h
@@ -1,9 +1,9 @@
-#define _IO_TMSRAM  0x80
-#define _IO_TMSREG  0x81
+#define IO_TMSRAM  0x80
+#define IO_TMSREG  0x81
 
-#define _IO_JOY0    0xA8
-#define _IO_JOY1    0xA9
+#define IO_JOY0    0xA8
+#define IO_JOY1    0xA9
 
-#define _IO_AYREG   0xB0
-#define _IO_AYDATA  0xB1
+#define IO_AYREG   0xB0
+#define IO_AYDATA  0xB1
 
