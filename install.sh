0daaa4a700fd8c985083e4e359c330314299fb2e
productiondave <kiwilathams@gmail.com>
2025-11-03 10:48:58 +1300

    standardize on ARCH naming - all capitals in all the places

--- install.sh
+++ install.sh
@@ -13,32 +13,32 @@ fi
 echo "Installing to $1"
 
 echo "Creating install directory..."
-mkdir -pv $1/lib/arch/retro
-mkdir -pv $1/lib/arch/nouveau
-mkdir -pv $1/lib/arch/nabu
-mkdir -pv $1/examples/arch/retro/skeleton
-mkdir -pv $1/examples/arch/nouveau/skeleton
-mkdir -pv $1/examples/arch/nabu/skeleton
+mkdir -pv $1/lib/arch/RETRO
+mkdir -pv $1/lib/arch/NOUVEAU
+mkdir -pv $1/lib/arch/NABU
+mkdir -pv $1/examples/arch/RETRO/skeleton
+mkdir -pv $1/examples/arch/NOUVEAU/skeleton
+mkdir -pv $1/examples/arch/NABU/skeleton
 mkdir -pv $1/include
 
 echo "Copying files..."
-cp -fv build/arch/RETRO/libcpm.a $1/lib/arch/retro/
-cp -fv build/arch/RETRO/crt0.o $1/lib/arch/retro/
-cp -fv build/arch/NOUVEAU/libcpm.a $1/lib/arch/nouveau/
-cp -fv build/arch/NOUVEAU/crt0.o $1/lib/arch/nouveau/
-cp -fv build/arch/NABU/libcpm.a $1/lib/arch/nabu/
-cp -fv build/arch/NABU/crt0.o $1/lib/arch/nabu/
+cp -fv build/arch/RETRO/libcpm.a $1/lib/arch/RETRO/
+cp -fv build/arch/RETRO/crt0.o $1/lib/arch/RETRO/
+cp -fv build/arch/NOUVEAU/libcpm.a $1/lib/arch/NOUVEAU/
+cp -fv build/arch/NOUVEAU/crt0.o $1/lib/arch/NOUVEAU/
+cp -fv build/arch/NABU/libcpm.a $1/lib/arch/NABU/
+cp -fv build/arch/NABU/crt0.o $1/lib/arch/NABU/
 cp -rv include/* $1/include/
 
 sed "s#INSTALLDIR#$1#" arch/NOUVEAU/skeleton/Makefile.template > arch/NOUVEAU/skeleton/Makefile
-cp -rv arch/NOUVEAU/skeleton/Makefile $1/examples/arch/nouveau/skeleton/
-cp -rv arch/NOUVEAU/skeleton/main.c $1/examples/arch/nouveau/skeleton/
+cp -rv arch/NOUVEAU/skeleton/Makefile $1/examples/arch/NOUVEAU/skeleton/
+cp -rv arch/NOUVEAU/skeleton/main.c $1/examples/arch/NOUVEAU/skeleton/
 
 sed "s#INSTALLDIR#$1#" arch/RETRO/skeleton/Makefile.template > arch/RETRO/skeleton/Makefile
-cp -rv arch/RETRO/skeleton/Makefile $1/examples/arch/retro/skeleton/
-cp -rv arch/RETRO/skeleton/main.c $1/examples/arch/retro/skeleton/
+cp -rv arch/RETRO/skeleton/Makefile $1/examples/arch/RETRO/skeleton/
+cp -rv arch/RETRO/skeleton/main.c $1/examples/arch/RETRO/skeleton/
 
 sed "s#INSTALLDIR#$1#" arch/NABU/skeleton/Makefile.template > arch/NABU/skeleton/Makefile
-cp -rv arch/NABU/skeleton/Makefile $1/examples/arch/nabu/skeleton/
-cp -rv arch/NABU/skeleton/main.c $1/examples/arch/nabu/skeleton/
+cp -rv arch/NABU/skeleton/Makefile $1/examples/arch/NABU/skeleton/
+cp -rv arch/NABU/skeleton/main.c $1/examples/arch/NABU/skeleton/
 
